015c852cc44ec8eae69551c094b81ad4439801f4
Laurence Luo <iluo@163.com>
2021-03-01 11:40:19 +0000

    Upgrade to 2.0 (4)

--- app/src/main/java/top/rainysummer/rootchecker/MainActivity.java
+++ app/src/main/java/top/rainysummer/rootchecker/MainActivity.java
@@ -9,6 +9,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.lahm.library.EasyProtectorLib;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -34,33 +40,82 @@ public class MainActivity extends AppCompatActivity {
         }
 
         TextView txtRoot = findViewById(R.id.txtRoot);
+        TextView txtRoot2 = findViewById(R.id.txtRoot2);
         TextView txtXposed = findViewById(R.id.txtXposed);
+        TextView txtXposed2 = findViewById(R.id.txtXposed2);
         TextView txtEmulator = findViewById(R.id.txtEmulator);
-        String strRoot, strXposed, strEmulator;
+        TextView txtEmulator2 = findViewById(R.id.txtEmulator2);
+        String strRoot, strRoot2, strXposed, strXposed2, strEmulator, strEmulator2, strSu;
         if (isRoot) {
-            strRoot = "\u274E <b><tt>Root</tt></b>: <font color=\"#FF4444\">Rooted</font>";
+            strRoot = "\u274E <b><tt>Root</tt></b>: ";
+            strRoot2 = "<font color=\"#FF4444\">" + getResources().getText(R.string.RootY) + "</font>";
         } else {
-            strRoot = "\u2705 <b><tt>Root</tt></b>: <font color=\"#99CC00\">Not Rooted</font>";
+            strRoot = "\u2705 <b><tt>Root</tt></b>: ";
+            strRoot2 = "<font color=\"#99CC00\">" + getResources().getText(R.string.RootN) + "</font>";
         }
         if (isXposed) {
-            strXposed = "\u274E <b><tt>Xposed</tt></b>: <font color=\"#FF4444\">Xposed Detected</font>";
+            strXposed = "\u274E <b><tt>Xposed</tt></b>: ";
+            strXposed2 = "<font color=\"#FF4444\">" + getResources().getText(R.string.XposedY) + "</font>";
         } else {
-            strXposed = "\u2705 <b><tt>Xposed</tt></b>: <font color=\"#99CC00\">Xposed Not Detected</font>";
+            strXposed = "\u2705 <b><tt>Xposed</tt></b>: ";
+            strXposed2 = "<font color=\"#99CC00\">" + getResources().getText(R.string.XposedN) + "</font>";
         }
         if (isEmulator) {
-            strEmulator = "\u274E <b><tt>Emulator</tt></b>: <font color=\"#FF4444\">Emulator Detected</font>";
+            strEmulator = "\u274E <b><tt>Emulator</tt></b>: ";
+            strEmulator2 = "<font color=\"#FF4444\">" + getResources().getText(R.string.EmulatorY) + "</font>";
         } else {
-            strEmulator = "\u2705 <b><tt>Emulator</tt></b>: <font color=\"#99CC00\">Not in Emulator Environment</font>";
+            strEmulator = "\u2705 <b><tt>Emulator</tt></b>: ";
+            strEmulator2 = "<font color=\"#99CC00\">" + getResources().getText(R.string.EmulatorN) + "</font>";
+        }
+        if (isRoot) {
+            strSu = whichSU();
+            strRoot2 += " (" + strSu + ")";
         }
         txtRoot.setText(Html.fromHtml(strRoot));
+        txtRoot2.setText(Html.fromHtml(strRoot2));
         txtXposed.setText(Html.fromHtml(strXposed));
+        txtXposed2.setText(Html.fromHtml(strXposed2));
         txtEmulator.setText(Html.fromHtml(strEmulator));
+        txtEmulator2.setText(Html.fromHtml(strEmulator2));
 
         TextView txtStatus = findViewById(R.id.txtStatus);
+
+
+
         if (isRoot || isXposed || isEmulator) {
             txtStatus.setText(Html.fromHtml("<font color=\"#FF4444\">Failed!</font>"));
         } else {
             txtStatus.setText(Html.fromHtml("<font color=\"#99CC00\">Passed!</font>"));
+        }
+    }
+
+    public static ArrayList<String> executeCommand(String[] shellCmd){
+        String line;
+        ArrayList<String> fullResponse = new ArrayList<>();
+        Process localProcess;
+        try {
+            localProcess = Runtime.getRuntime().exec(shellCmd);
+        } catch (Exception e) {
+            return null;
+        }
+        BufferedReader in = new BufferedReader(new InputStreamReader(localProcess.getInputStream()));
+        try {
+            while ((line = in.readLine()) != null) {
+                fullResponse.add(line);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return fullResponse;
+    }
+
+    public static String whichSU() {
+        String[] strCmd = new String[] {"/system/xbin/which","su"};
+        ArrayList<String> execResult = executeCommand(strCmd);
+        if (execResult != null){
+            return execResult.get(0);
+        }else{
+            return null;
         }
     }
 }
